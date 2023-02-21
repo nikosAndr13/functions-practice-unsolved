06cf836f0b8828283656f584bef4967188b9cbf2
Nikos Andriopoulos <nickos2014andriopoulos@gmail.com>
2023-02-21 16:46:31 +0200

    Refactor e4 last functions

--- exercises/e4.js
+++ exercises/e4.js
@@ -41,9 +41,9 @@ const isNameInArray = (arr, name) => arr.includes(name);
  * Hint: There are two functions to refactor.
  */
 
-const logSecondsUpToMax = function (max) {
+const logSecondsUpToMax = (max) => {
   let i = 0;
-  const timer = setInterval(function() {
+  const timer = setInterval(() => {
     if (i < max) {
       console.log(i++);
     }
